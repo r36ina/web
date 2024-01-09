0f51ee139c35b5760920333e7fa902305b1ef0ca
r36ina <150552148+r36ina@users.noreply.github.com>
2024-01-10 02:15:47 +0500

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<fieldset class="container" id="signInContainer">
-        <form class="login" action="send.php" method="post">
-            <legend class="login-title">Sign In</legend>
-            <input type="text" name="name" id="name" pattern="[A-Za-zA-Яа-яЁё\s]+" placeholder="Enter Name" title="Name must contain only letters" required>
-            <input type="password" name="password" placeholder="Enter Password">
-            <input type="submit" value="Sign In">
-        </form>
-    </fieldset>
